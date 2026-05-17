8dc86d8e1077a33f568a731361bd7acbb0dbbc56
RollaDevdfirst <ajayiezekiel348@gmail.com>
2026-05-17 06:04:30 +0100

    Added a little change to test my repo

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Backend is runnig🚀");
+    res.send("Backend is runnig right now🚀");
 }) ;
 
 const PORT = process.env.PORT || 5000;
